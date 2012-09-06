29159103b7d637b346f8fbd0ffb2b5baf66bd63c
Graylin Kim <graylin.kim@gmail.com>
2012-09-06 10:56:11 -0400

    Only use max-results:10,000 when not configured.

--- src/main/java/gov/nysenate/analytics/connectors/GoogleAnalyticsConnect.java
+++ src/main/java/gov/nysenate/analytics/connectors/GoogleAnalyticsConnect.java
@@ -14,17 +14,17 @@ import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 
 public class GoogleAnalyticsConnect {
-	
-	private Section config;
+
+	private final Section config;
 	private AnalyticsService as;
-	
+
 	public String getDateString(String date_type, Section params) {
 		return params.containsKey(date_type) ? params.get(date_type) : config.get(date_type);
 	}
-	
+
 	public GoogleAnalyticsConnect(Section config) {
 		this.config = config;
-		
+
 		try {
 			as = new AnalyticsService(config.get("app_name"));
 			as.setUserCredentials(config.get("user"), config.get("pass"));
@@ -44,22 +44,23 @@ public class GoogleAnalyticsConnect {
             System.err.println("Network error trying to retrieve feed: " + e.getMessage());
         } catch (ServiceException e) {
             System.err.println("Analytics API responded with an error message: " + e.getMessage());
-        }	
+        }
 		return null;
 	}
-	
+
 	public DataFeed getDataFeed(Section params) {
 		try {
 			DataQuery query = new DataQuery(new URL(config.get("data_url")));
-			
+
 			if(params.get("max_results") != null)
 				query.setMaxResults(Integer.parseInt(params.get("max_results")));
-			
+			else
+			    query.setMaxResults(10000); // The max it can handle
+
 			query.setIds("ga:"+params.get("id"));
-			query.setMaxResults(10000); // The max it can handle
 			query.setStartDate(this.getDateString("start_date", params));
 			query.setEndDate(this.getDateString("end_date", params));
-			
+
 			if(params.containsKey("dimensions"))
 				query.setDimensions(params.get("dimensions"));
 			if(params.containsKey("metrics"))
@@ -68,9 +69,9 @@ public class GoogleAnalyticsConnect {
 				query.setSort(params.get("sort"));
 			if(params.containsKey("filters"))
 				query.setFilters(params.get("filters"));
-			
+
 			return as.getFeed(query, DataFeed.class);
-			
+
 		} catch (MalformedURLException e) {
 			System.err.println("Malformed URL: " + config.get("data_url"));
 		} catch (IOException e) {
@@ -79,5 +80,5 @@ public class GoogleAnalyticsConnect {
             System.err.println("Analytics API responded with an error message: " + e.getMessage());
         }
 		return null;
-	}	
+	}
 }
